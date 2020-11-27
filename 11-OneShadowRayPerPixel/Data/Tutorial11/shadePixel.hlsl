bd96815827e86bf4788a24f29871c6b1a0a04072
Songsong97 <694118974@qq.com>
2020-11-27 10:55:56 +0800

    Update shadePixel.hlsl
    shadePixel

--- 11-OneShadowRayPerPixel/Data/Tutorial11/shadePixel.hlsl
+++ 11-OneShadowRayPerPixel/Data/Tutorial11/shadePixel.hlsl
@@ -10,14 +10,36 @@ RWTexture2D<float4> gMatEmissive;
 
 // Reservoir texture
 RWTexture2D<int> sampleIndex;
+RWTexture2D<float4> emittedLight;
 RWTexture2D<float4> toSample; // xyz: hit to sample // w: distToLight
 RWTexture2D<float4> sampleNormalArea; // xyz: sample noraml // w: area of light
 RWTexture2D<float4> reservoir; // x: W // y: Wsum // zw: not used
 RWTexture2D<int> M;
 
+cbuffer {
+	uint lightCount;
+};
+
 float4 main(float2 texC : TEXCOORD, float4 pos : SV_Position) : SV_Target0
 {
 	uint2 pixelPos = (uint2)pos.xy; // Where is this pixel on screen?
 
-	return gWsNorm[pixelPos];
+	float lambert = max(0, dot(toSample[pixelPos].xyz, gWsNorm[pixelPos].xyz));
+	float3 bsdf = gMatDif[pixelPos].xyz / 3.14159265359f;
+	float3 L = emittedLight[pixelPos].xyz;
+	
+	// Compute pdf of sampling light
+	float pdfL = 0.f;
+	float cosLight = dot(-toSample[pixelPos].xyz, sampleNormalArea[pixelPos].xyz));
+	if (cosLight != 0.f && sampleNormalArea[pixelPos].w > 0) {
+		float r = toSample[pixelPos].w;
+		pdfL = r * r / (cosLight * sampleNormalArea[pixelPos].w);
+	}
+	if (pdfL == 0.f) {
+		return float4(0, 0, 0, 1.f);
+	}
+	else {
+		return (bsdf * L) * lambert * lightCount / pdfL * reservoir[pixelPos].x;
+	}
+	return f;
 }
