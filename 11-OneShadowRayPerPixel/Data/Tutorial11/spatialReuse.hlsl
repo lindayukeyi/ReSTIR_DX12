f635ebbfdd3055c04e31c8678b9fc89459c57b70
Jilin <694118974@qq.com>
2020-11-30 07:11:31 +0800

    Use random helper

--- 11-OneShadowRayPerPixel/Data/Tutorial11/spatialReuse.hlsl
+++ 11-OneShadowRayPerPixel/Data/Tutorial11/spatialReuse.hlsl
@@ -3,6 +3,8 @@
 // Include helper functions
 // #include "diffusePlus1ShadowUtils.hlsli"
 #include "pbr.hlsli"
+#include "randomHelper.hlsli"
+
 static const float PI = 3.14159265f;
 
 // G-Buffer
@@ -33,29 +35,6 @@ struct Pingpong
 	float4 pemittedLight     : SV_Target3;
 	int    pM                : SV_Target4;
 };
-
-
-/**********************************************************************/
-uint initRand(uint val0, uint val1, uint backoff = 16)
-{
-	uint v0 = val0, v1 = val1, s0 = 0;
-
-	[unroll]
-	for (uint n = 0; n < backoff; n++)
-	{
-		s0 += 0x9e3779b9;
-		v0 += ((v1 << 4) + 0xa341316c) ^ (v1 + s0) ^ ((v1 >> 5) + 0xc8013ea4);
-		v1 += ((v0 << 4) + 0xad90777d) ^ (v0 + s0) ^ ((v0 >> 5) + 0x7e95761e);
-	}
-	return v0;
-}
-
-float nextRand(inout uint s)
-{
-	s = (1664525u * s + 1013904223u);
-	return float(s & 0x00FFFFFF) / float(0x01000000);
-}
-/**********************************************************************/
 
 void updatereservoir(float3 le, float4 tos, float4 sna, float w, inout uint seed, inout Pingpong pp) {
 	pp.preservoir.y += w;  // wsum += w
