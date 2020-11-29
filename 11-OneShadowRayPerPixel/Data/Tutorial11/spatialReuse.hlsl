7ef52dfbc362e6a50ecf244e79a56ffb96e44007
Jilin <694118974@qq.com>
2020-11-29 17:16:06 +0800

    fix some errors

--- 11-OneShadowRayPerPixel/Data/Tutorial11/spatialReuse.hlsl
+++ 11-OneShadowRayPerPixel/Data/Tutorial11/spatialReuse.hlsl
@@ -1,8 +1,8 @@
 /* Spatial Reuse */
 
 // Include helper functions
-//#include "diffusePlus1ShadowUtils.hlsli"
-
+// #include "diffusePlus1ShadowUtils.hlsli"
+#include "pbr.hlsli"
 static const float PI = 3.14159265f;
 
 // G-Buffer
@@ -55,16 +55,9 @@ float nextRand(inout uint s)
 	s = (1664525u * s + 1013904223u);
 	return float(s & 0x00FFFFFF) / float(0x01000000);
 }
-
-float evalP(float3 toLight, float3 diffuse, float3 energy, float3 nor) {
-	float lambert = saturate(dot(toLight, nor));
-	float3 brdf = diffuse / 3.1415926535898f;
-	float3 color = brdf * energy * lambert;
-	return length(color);
-}
 /**********************************************************************/
 
-void updatereservoir(float3 le, float4 tos, float4 sna, float w, inout uint seed, Pingpong pp) {
+void updatereservoir(float3 le, float4 tos, float4 sna, float w, inout uint seed, inout Pingpong pp) {
 	pp.preservoir.y += w;  // wsum += w
 	pp.pM = pp.pM + 1;
 	if (nextRand(seed) < (w / pp.preservoir.y)) {
@@ -72,6 +65,7 @@ void updatereservoir(float3 le, float4 tos, float4 sna, float w, inout uint seed
 		pp.ptoSample = tos;
 		pp.psampleNormalArea = sna;
 	}
+	return;
 }
 
 Pingpong main(float2 texC : TEXCOORD, float4 pos : SV_Position)
@@ -91,36 +85,43 @@ Pingpong main(float2 texC : TEXCOORD, float4 pos : SV_Position)
 	pp.pemittedLight = emittedLight[pixelPos];
 	pp.pM = M[pixelPos];
 
+	int i_total = 0;
+	// Sample k = 5 (k = 3 for our unbiased algorithm) random points in a 30 - pixel radius around the current pixel
 	for (int i = 0; i < 5; i++) {
-	// sample k = 5 (k = 3 for our unbiased algorithm) random points in a 30 - pixel radius around the current pixel
+		if (i_total > 10) {
+			break;
+		}	  
 		float r = 30.0 * sqrt(nextRand(seed));
 		float theta = 2.0 * PI * nextRand(seed);
 		uint2 neighborPos;
 		neighborPos.x = pixelPos.x + int(clamp(r * cos(theta), 0.0, width));
 		neighborPos.y = pixelPos.y + int(clamp(r * sin(theta), 0.0, height));
-
+		// The angle between normals of the current pixel to the neighboring pixel exceeds 25 degree
+		if ( dot( normalize(gWsNorm[pixelPos].xyz), normalize(gWsNorm[neighborPos].xyz) ) < 0.9063) {
+			i--;
+			i_total++;
+			continue;
+		}
+		// Exceed 10% of current pixel's depth
+		//if (gWsPos[neighborPos].z > 1.1 * gWsPos[pixelPos].z || gWsPos[neighborPos].z < 0.9 * gWsPos[pixelPos].z) {
+		//	i--;
+		//	i_total++;
+		//	continue;
+		//}
 		float3 nor = normalize(gWsNorm[neighborPos].xyz);
 		float p_hat = evalP(toSample[neighborPos].xyz, gMatDif[neighborPos].xyz, emittedLight[neighborPos].xyz, nor);
 		float3 Le = emittedLight[neighborPos].xyz;
 		float4 toS = toSample[neighborPos];
 		float4 sNA = sampleNormalArea[neighborPos];
 		float w = p_hat * reservoir[neighborPos].x * M[neighborPos];
-		////////////////////////////////
-		pp.preservoir.y += w;  // wsum += w
-		pp.pM = pp.pM + 1;
-		if (nextRand(seed) < (w / pp.preservoir.y)) {
-			pp.pemittedLight = float4(Le, 1.f);
-			pp.ptoSample = toS;
-			pp.psampleNormalArea = sNA;
-		}
-		//////////////////////////////////updatereservoir(Le, toS, sNA, w, seed, pp);
+		updatereservoir(Le, toS, sNA, w, seed, pp);
 		M_sum += M[neighborPos];
 	}
 
 	pp.pM = M_sum;
 	float3 nor_s = normalize(gWsNorm[pixelPos].xyz);
-	float p_hat_s = evalP(toSample[pixelPos].xyz, gMatDif[pixelPos].xyz, emittedLight[pixelPos].xyz, nor_s);
-	pp.preservoir.x = reservoir[pixelPos].y / p_hat_s / M_sum;
+	float p_hat_s = evalP(pp.ptoSample.xyz, gMatDif[pixelPos].xyz, pp.pemittedLight.xyz, nor_s);
+	pp.preservoir.x = pp.preservoir.y / p_hat_s / float(pp.pM);
 
 	return pp;
 }
--- 11-OneShadowRayPerPixel/Tutor11-OneShadowRayPerPixel.cpp
+++ 11-OneShadowRayPerPixel/Tutor11-OneShadowRayPerPixel.cpp
@@ -35,10 +35,10 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	pipeline->setPass(0, RayTracedGBufferPass::create());  // generate G-buffer and initial candidates
 	pipeline->setPass(1, ShadowDetectionPass::create());    // remove invisible sample
 	pipeline->setPass(3, SpatialReusePass::create());       // spatial reuse
-	//pipeline->setPass(4, SpatialReusePass::create());       // spatial reuse
-	//pipeline->setPass(5, SpatialReusePass::create());       // spatial reuse
-	//pipeline->setPass(6, SpatialReusePass::create());       // spatial reuse
-	//pipeline->setPass(7, SpatialReusePass::create());       // spatial reuse
+	pipeline->setPass(4, SpatialReusePass::create());       // spatial reuse
+	pipeline->setPass(5, SpatialReusePass::create());       // spatial reuse
+	pipeline->setPass(6, SpatialReusePass::create());       // spatial reuse
+	pipeline->setPass(7, SpatialReusePass::create());       // spatial reuse
 	pipeline->setPass(8, ShadePixelPass::create());         // compute final color
 	pipeline->setPass(9, CopyToOutputPass::create());       // output selected texture to channel; for debug
 
